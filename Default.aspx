552e92ced7f1751837fd51ead20f614399555739
HargulS <hargul.sidhu@gmail.com>
2021-11-03 01:34:19 -0700

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="font-size:x-large; height: 97px;" align="center" > 
+        <br />
+        Student Information<br />
+        
+    </div>
+
+&nbsp;<table class="nav-justified">
+    <tr>
+        <td style="width: 351px; height: 20px;"></td>
+        <td class="modal-sm" style="width: 125px; height: 20px;">Student ID</td>
+        <td style="height: 20px">
+            <asp:TextBox ID="TextBox1" runat="server" style="margin-left: 0" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged1"></asp:TextBox>
+            <asp:Button ID="Button5" runat="server" style="margin-left:10px" Text="Get Record" Width="126px" OnClick="Button5_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 351px; height: 22px;"></td>
+        <td class="modal-sm" style="width: 125px; height: 22px;">Student Name</td>
+        <td style="height: 22px">
+            <asp:TextBox ID="TextBox2" runat="server" style="margin-left: 0" AutoPostBack="True"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 351px">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px">Address</td>
+        <td>
+            <asp:DropDownList ID="DropDownList1" runat="server" Height="25px" Width="126px" AutoPostBack="True">
+                <asp:ListItem>USA</asp:ListItem>
+                <asp:ListItem>Canada</asp:ListItem>
+                <asp:ListItem>UK</asp:ListItem>
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 351px">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px">Age</td>
+        <td class="modal-sm" style="width: 125px">
+            <asp:TextBox ID="TextBox3" runat="server" style="margin-left: 0" AutoPostBack="True"></asp:TextBox>
+        </td>
+        <td>
+            &nbsp;</td>
+    </tr>
+    <tr>
+        <td style="width: 351px">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px">Contact</td>
+        <td>
+            <asp:TextBox ID="TextBox4" runat="server" style="margin-left: 0" AutoPostBack="True"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 351px">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px">&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td style="width: 351px">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px">&nbsp;</td>
+        <td>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" Width="126px" />
+            <asp:Button ID="Button2" runat="server" style="margin-left:10px" Text="Update" Width="126px" OnClick="Button2_Click1" />
+            <asp:Button ID="Button3" runat="server" style="margin-left:10px" OnClientClick="return confirm('Are you sure you want to Delete this record?')" Text="Delete" Width="126px" OnClick="Button3_Click1" />
+            <asp:Button ID="Button4" runat="server" style="margin-left:10px" Text="Search" Width="126px" OnClick="Button4_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 351px; height: 20px;"></td>
+        <td class="modal-sm" style="width: 125px; height: 20px;"></td>
+        <td style="height: 20px"></td>
+    </tr>
+    <tr>
+        <td style="width: 351px; height: 20px;">&nbsp;</td>
+        <td class="modal-sm" style="width: 125px; height: 20px;">&nbsp;</td>
+        <td style="height: 20px">
+            <asp:GridView ID="GridView1" runat="server" Width="640px">
+            </asp:GridView>
+        </td>
+    </tr>
+</table>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+      
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+    
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        protected void TextBox1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+             SqlConnection con = new SqlConnection("Data Source=DESKTOP-KJ3D7HA\\SQLEXPRESS;Initial Catalog=StudentRecords;Integrated Security=True");
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+
+            con.Open();
+            // Sql connection string
+            SqlCommand comm = new SqlCommand("Insert into dbo.StudentInfo_Tab values('" + int.Parse(TextBox1.Text) + "','" + TextBox2.Text + "','" + DropDownList1.SelectedValue + "','" + double.Parse(TextBox3.Text) + "','" + TextBox4.Text + "')", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            LoadRecord();
+
+        }
+        //To show inserted values in the grid view
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("select * from StudentInfo_Tab",con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+            con.Open();
+            //Sql connection string
+           SqlCommand comm = new SqlCommand("update dbo.StudentInfo_Tab set StudentName='" + TextBox2.Text + "', Address='" + DropDownList1.SelectedValue + "', Age='" + double.Parse(TextBox3.Text) + "', Contact='" + TextBox4.Text + "' where StudentId = '" + int.Parse(TextBox1.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+            LoadRecord();
+        }
+
+        protected void Button3_Click1(object sender, EventArgs e)
+        {
+            con.Open();
+            //Sql connection string
+            SqlCommand comm = new SqlCommand("Delete dbo.StudentInfo_Tab where StudentId = '" + int.Parse(TextBox1.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+            LoadRecord();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("select * from StudentInfo_Tab where StudentId = '" + int.Parse(TextBox1.Text) + "'", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("select * from StudentInfo_Tab where StudentId = '" + int.Parse(TextBox1.Text) + "'", con);
+            SqlDataReader r = comm.ExecuteReader();
+            while (r.Read())
+            {
+                TextBox2.Text = r.GetValue(1).ToString();
+                DropDownList1.SelectedValue = r.GetValue(2).ToString();
+                TextBox3.Text = r.GetValue(3).ToString();
+                TextBox4.Text = r.GetValue(4).ToString();
+            }
+        }
+    }
+}
